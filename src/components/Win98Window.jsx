eb98e01123da455f7f070b5def2a8450f3a1ee20
RaphaelMadeira-design <raphael.m21@gmail.com>
2026-04-11 19:19:52 +0200

    Speed

--- src/components/Win98Window.jsx
+++ src/components/Win98Window.jsx
@@ -4,7 +4,6 @@ import { Rnd } from 'react-rnd'
 const ICONS = {
   powers: 'https://win98icons.alexmeub.com/icons/png/executable_script-0.png',
   documents: 'https://win98icons.alexmeub.com/icons/png/directory_closed-4.png',
-  notepad: 'https://win98icons.alexmeub.com/icons/png/notepad-0.png',
   snake: 'https://win98icons.alexmeub.com/icons/png/executable-0.png',
   jump: 'https://win98icons.alexmeub.com/icons/png/executable-0.png',
   media: 'https://win98icons.alexmeub.com/icons/png/wm-3.png',
